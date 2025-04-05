57efe27dae2906aeacf6059e5f92792ce41c47bc
GabryelMelo07 <gabryelcraft@gmail.com>
2025-04-05 02:30:03 -0300

    feat: adjusting image component

--- components/common/TableProductImage.tsx
+++ components/common/TableProductImage.tsx
@@ -2,20 +2,30 @@ import Image, { StaticImageData } from "next/image";
 import { CiImageOff } from "react-icons/ci";
 
 interface Props {
-    src?: string | StaticImageData | undefined | null;
-    alt?: string;
+  src?: string | StaticImageData | undefined | null;
+  alt?: string;
 }
 
-export default function TableProductImage({src, alt = ""}: Props) {
-    return (
-
-        <div className="flex justify-center w-full">
-            {
-                src ? 
-                <Image src={src} alt={alt} width={35} height={35} className="rounded-md border border-[rgba(0,0,0,0.09)]"/>  :
-                <span className="text-sm text-gray-900 italic"><CiImageOff  size={35}/></span>
-            }
-        </div>
-
-    )
+export default function TableProductImage({ src, alt = "" }: Props) {
+  return src ? (
+    <div className="rounded-md outline outline-2 outline-solid outline-[rgba(0,0,0,0.8)] items-center w-8 h-8">
+      {
+        <Image
+          src={src}
+          alt={alt}
+          width={32}
+          height={32}
+          className="rounded-md object-contain max-w-full max-h-full"
+        />
+      }
+    </div>
+  ) : (
+    <div className="rounded-md outline outline-2 outline-solid outline-[rgba(0,0,0,0.8)] items-center w-8 h-8">
+      {
+        <span className="text-sm text-gray-900 italic">
+          <CiImageOff size={32} />
+        </span>
+      }
+    </div>
+  )
 }
