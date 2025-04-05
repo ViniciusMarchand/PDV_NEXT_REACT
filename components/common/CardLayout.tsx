d6afeb5de40bdbe5b0d5f198a402a1f4aaa58def
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2025-04-05 14:40:14 -0300

    fix: layout

--- components/common/CardLayout.tsx
+++ components/common/CardLayout.tsx
@@ -1,9 +1,12 @@
+import { cn } from "@/lib/utils"
+
 type Props = {
-    children: string | JSX.Element | JSX.Element[]
+    children: string | JSX.Element | JSX.Element[],
+    className?: string,
 }
-export default function CardLayout({children} : Props) {
+export default function CardLayout({children, className} : Props) {
     return (
-        <div className="bg-secundaria rounded-md shadow-lg h-full w-full flex flex-col">
+        <div className={cn("bg-secundaria rounded-md shadow-lg h-full w-full flex flex-col", className)}>
             {children}
         </div>
     )
