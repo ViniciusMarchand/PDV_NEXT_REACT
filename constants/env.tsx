f2c0db7d84e3bcc94bc760044c324299fa19f7af
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2025-04-23 16:31:09 -0300

    fix: bugs

--- constants/env.tsx
+++ constants/env.tsx
@@ -14,6 +14,6 @@ export const clientInfo = {
 export const pixInfo = {
     key: process.env.NEXT_PUBLIC_PIX_KEY || '',
     name: process.env.NEXT_PUBLIC_PIX_NAME || '',
-    city: process.env.NEXT_PUBLIC_PIX_CITY || '',
+    city: process.env.NEXT_PUBLIC_CIDADE || '',
     transactionId: process.env.NEXT_PUBLIC_PIX_TRANSACTION_ID || ''
 }
